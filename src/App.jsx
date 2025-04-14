8a109e8fbf98cece67ce43eb77404194db3a64ec
Julio Caniunir <julio.caniunir@estelarbet.com>
2025-04-14 13:23:19 -0400

    julito uwu

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
-import './App.css'
-import ContentContainer from './ContentContainer/ContentContainer'
+import './App.css';
+import ContentContainer from './ContentContainer/ContentContainer';
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RolePage from "./RolePage";
 import NotFound from "./NotFound";
+
 function App() {
   return (
-    <>
-<Router>
+    <Router>
       <Routes>
+        {/* Ruta para la página principal */}
+        <Route path="/" element={<ContentContainer />} />
+
+        {/* Ruta dinámica */}
         <Route path="/role/:type" element={<RolePage />} />
+
+        {/* Ruta para cualquier otra (404) */}
         <Route path="*" element={<NotFound />} />
       </Routes>
-      
-</Router>
-    </>
-  )
+    </Router>
+  );
 }
-export default App
+
+export default App;
