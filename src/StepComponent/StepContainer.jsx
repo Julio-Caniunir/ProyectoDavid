b13566c5f75ec52d8b00e51a76a1242c16f55a8f
Julio Caniunir <julio.caniunir@estelarbet.com>
2025-04-14 13:14:07 -0400

    julito uwu

--- src/StepComponent/StepContainer.jsx
+++ src/StepComponent/StepContainer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import Search from '../search/search'
+import Search from '../search/Search';
 import SearchDesktop from '../search/SearchDesktop'
 import useIsMobile from '../Hooks/useIsMobile'
 import './StepContainer.css';
